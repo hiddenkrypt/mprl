2ae6501f1ee6df8362e32b9e5e1202a8e5b4a6c2
hiddenkrypt <hiddenkrypt@gmail.com>
2018-06-09 14:33:22 -0700

    fixed module references

--- titan.js
+++ titan.js
@@ -1,10 +1,11 @@
 //titan.js
 "use strict"
-var world = require("world.js")
-var ui = require("ui.js")
+var world = require("./world.js")()
+var ui = require("./ui.js")(world)
 
 module.exports = {
   newPlayer: function newPlayer(newSocket){
     ui.connectNewPlayer(newSocket)
     console.log("added new player socket to array")
+  }
 }

271c260afb55f87779fa99cef06889e2b1a44b24
hiddenkrypt <hiddenkrypt@gmail.com>
2018-06-30 13:19:22 -0700

    using jsonfile package to grab beastiary

--- summoning.js
+++ summoning.js
@@ -1,6 +1,15 @@
-const beastiary = {};
-
+const jsonfile = require('jsonfile');
+console.log("summoning.js || Loading beastiary.json");
+try{
+  const beastiary = jsonfile.readFileSync('./beastiary.json');
+}
+catch(err){
+  console.error(`summoning.js || Attempt to read beastiar yfailed! Full details below:`);
+  console.log(err);
+}
+console.log("summoning.js || beastiary.json loaded");
 module.exports = (function(){
+
   this.create = function(type, id){
     var newEntity = { id: id };
   };

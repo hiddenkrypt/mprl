2a62107628505603ee64344d6b164433803b3797
hiddenkrypt <hiddenkrypt@gmail.com>
2018-06-30 11:05:16 -0700

    refactored as an IIFE, using entity manager for player spawn

--- ui.js
+++ ui.js
@@ -1,27 +1,27 @@
 console.log("load UI module");
 var players = [];
 var world;
+var entityManager;
 
-function Player(newSocket){
+function Player(newSocket, newCharacter){
   this.socket = newSocket;
   this.username = "";
-  this.character = null;
-  this.position = { x:-1, y:-1};
+  this.character = newCharacter;
 }
 
-module.exports = function(){
+module.exports = (function(){
   console.log("init ui module");
-  return {
-    setWorld: function(w){
-      world = w;
-    },
-    connectNewPlayer: function(socket){
-      var newPlayer = new Player(socket);
-      newPlayer.position = world.spawnPlayer();
-      playerIndex = players.push(newPlayer)-1;
-
-      var playerMap = world.getMap(player);
-      socket.emit("world", playerMap);
-    }
+  this.setWorld = function( w ) {
+    world = w;
   };
-};
+  this.setEntityManager = function( ent ) {
+    entityManager = ent;
+  };
+  this.connectNewPlayer =  function( socket ) {
+    var newPlayer = new Player(socket, entityManager.spawn("player"));
+    playerIndex = players.push(newPlayer)-1;
+
+    var playerMap = world.getMap(player);
+    socket.emit("world", playerMap);
+  };
+})();
